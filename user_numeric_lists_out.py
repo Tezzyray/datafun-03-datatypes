09b7d57cdba36fedc7221097d08b45a6464aba1c
Tezzyray <122491469+Tezzyray@users.noreply.github.com>
2023-02-06 15:25:58 -0700

    Create user_numeric_lists_out.py

---
+++ user_numeric_lists_out.py
@@ -0,0 +1,28 @@
+the mean of list1 is: 39.55
+The median is : 32.5
+The mode is : 2
+The standard deviation is : 29.960456394528123
+The variance is : 897.6289473684211
+correlation between listx and listy: 0.9934722135633581
+The future y at futurex=15 is:  25
+maximum value in listy is: 24.6
+minimum value in listy is: 10.5
+the lenght of listy is: 10
+the sum of values in listy is:  183.1
+the average value for listy is:  18.31
+This is listy in descending order:  [24.6, 23.4, 22.5, 21.3, 19.5, 17.9, 16.5, 14.6, 12.3, 10.5]
+this is a new list with another value: {None}
+this is the extended list:  {None}
+list after inserting a new value: [40, 89, 100, 93, 90, 34, 300]
+this is the list after removing a value: [40, 89, 93, 90, 34, 300]
+89 appears on the list <built-in method count of list object at 0x000001A7AB204780> times
+This is the list in ascending order: [34, 40, 89, 89, 90, 93, 300]
+This is the list in descending order: [300, 93, 90, 89, 89, 40, 34]
+This is the copied list: [300, 93, 90, 89, 89, 40, 34]
+This is the list after removing first value from lst_new: 300
+This is the list after removing the last value: 34
+This is the list filtered for any value less than 4 [1, 2, 3]
+The cube root for all values in the list are: [1, 8, 27, 64, 125, 216, 343, 512]
+The volume of the cube is : [64]
+New list after filtering using comprehension is : [2, 5, 7]
+This is the triple value if list1: [6, 30, 90, 15, 21, 27, 36, 39, 162, 204, 228, 186, 96, 63, 99, 132, 180, 234, 294, 231]
