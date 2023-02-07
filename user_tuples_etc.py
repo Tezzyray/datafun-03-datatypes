e43311715dd0dd998031ea4a1b9fa5d2cab26723
tezzyray <olakunle.t.alabi@gmail.com>
2023-02-06 17:40:47 -0700

    project 3

--- user_tuples_etc.py
+++ user_tuples_etc.py
@@ -2,3 +2,26 @@
 Modify this docstring.
 
 """
+# Create some tuples
+Ad_ID= (2, 5, 6, 8, 10)
+Loc_ID = (5, 8, 10, 12, 14)
+
+
+# tuple concatenation
+ad_total = Ad_ID + Loc_ID
+
+
+#SETS
+
+Set1= {1,2,3,4,5}
+Set2={3,4,6,7,8}
+
+#union
+Set3= Set1 |Set2
+print('The union between both sets are:', Set3)
+
+#INTERSECTION
+
+Set4= Set1 & Set2
+print('The intersection between the sets are :', Set4)
+
