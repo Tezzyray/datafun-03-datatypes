3b52620bdbe54723a4bcaf0302561015a089867a
Tezzyray <122491469+Tezzyray@users.noreply.github.com>
2023-02-06 17:17:38 -0700

    Create user_string_lists_out.py

---
+++ user_string_lists_out.py
@@ -0,0 +1,12 @@
+The lenght of advert type is: 8
+The lenght of products is: 8
+the lenght of target audience is : 8
+the lenght of advert medium is:, 8
+the lenght of ad locations is:  8
+The set of ad locations are: {'schools', 'hotels', 'sports centers', 'airport', 'home', 'roadside', 'cinema', 'malls'}
+The zipped list are: <zip object at 0x000001CE6E525640>
+This is a random advert type suggested: Social Media
+
+
+Unique words was gotten from the simple file.
+The lenght of the unique words is: 6
